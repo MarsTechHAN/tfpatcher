bace32cef98ccf4dab68118eecba67e3d79ca80d
Martin Han <hansh-sz@hotmail.com>
2020-02-21 03:21:31 +0800

    Add Mean patch for RKNN with SE

--- frozenpb_patcher.py
+++ frozenpb_patcher.py
@@ -99,6 +99,55 @@ node {
 }
 '''
 
+MEAN_PATCH =\
+'''
+node {
+  name: "{NAME}"
+  op: "AvgPool"
+  input: "{INPUT}"
+  attr {
+    key: "T"
+    value {
+      type: DT_FLOAT
+    }
+  }
+  attr {
+    key: "data_format"
+    value {
+      s: "NHWC"
+    }
+  }
+  attr {
+    key: "ksize"
+    value {
+      list {
+        i: 1
+        i: {KERNEL_SIZE}
+        i: {KERNEL_SIZE}
+        i: 1
+      }
+    }
+  }
+  attr {
+    key: "padding"
+    value {
+      s: "VALID"
+    }
+  }
+  attr {
+    key: "strides"
+    value {
+      list {
+        i: 1
+        i: 1
+        i: 1
+        i: 1
+      }
+    }
+  }
+}
+'''
+
 EXPLICT_PAD_ATTR_REGEX = r'attr {\n[\s]+key: "explicit_paddings"\n[\s]'\
                       r'+value {\n[\s]+list {\n[\s]+}\n[\s]+}\n[\s]+}'
 
@@ -119,10 +168,82 @@ OUTPUT_SHAPE_REGEX_1 = r'attr {\n[\s]+key: "_output_shapes"\n[\s]+value {\n[\s]+
 OUTPUT_SHAPE_REGEX_2 = r'[\s]+attr {\n[\s]+key: "_output_shapes"\n[\s]+value {\n[\s]+list {\n[\s]+shape '\
                     r'{([\s]+dim[\s]+{\n[\s]+size:[\s]+[0-9]+\n[\s]+}\n)+([\s]+}\n)+'
 
+REDUCTION_IDENCE_REGEX = r'node[\s]+{\n[\s]+name:[\s]+\"[^"]+reduction_indices'\
+                      r'\"\n[\s]+op:[\s]+\"Const\"[\s]+[\s]+attr[\s]+{\n[\s]+k'\
+                      r'ey:[\s]+\"dtype\"\n[\s]+value[\s]+{\n[\s]+type:[\s]+DT'\
+                      r'_INT32\n[\s]+}\n[\s]+}\n[\s]+attr[\s]+{\n[\s]+key:[\s]'\
+                      r'+\"value\"\n[\s]+value[\s]+{\n[\s]+tensor[\s]+{\n[\s]+'\
+                      r'dtype:[\s]+DT_INT32\n[\s]+tensor_shape[\s]+{\n[\s]+dim'\
+                      r'[\s]+{\n[\s]+size:[\s]+2\n[\s]+}\n[\s]+}\n[\s]+tensor_'\
+                      r'content:[\s]+\"\\001\\000\\000\\000\\002\\000\\000\\00'\
+                      r'0\"\n+([\s]+}\n[\s]+)(}\n[\s]+}\n})'
+
 def pbtxt_processing(content):
     if content.find('explicit_paddings') != -1:
       print('Find unsupported attr: explicit_paddings, removing...\n')
       content = re.sub(EXPLICT_PAD_ATTR_REGEX, '', content)
+    
+    meanCounter = content.count('op: "Mean"')
+    if meanCounter > 1: #May not work
+      endMeanNodeName = ""
+      print(f'Find semi-supported op: Mean presented {meanCounter} times in pb, patching...')
+      while 1:
+        meanOpLoc = content.find('op: "Mean"')
+        if meanOpLoc == -1:
+          break
+        nodeNameLoc = content.rfind('name', 0, meanOpLoc)
+        nodeNameDLoc = content.find('"', nodeNameLoc)
+        nodeName = content[nodeNameDLoc+1:content.find('"', nodeNameDLoc + 1)]
+
+        nodeInputLoc = content.find('input', meanOpLoc)
+        nodeInputDLoc = content.find('"', nodeInputLoc)
+        nodeInputName = content[nodeInputDLoc+1:content.find('"', nodeInputDLoc + 1)]
+
+        inputNodeNameLoc = content.find(f'name: "{nodeInputName}"')
+        inputNodeEnd = content.find('node', inputNodeNameLoc)
+        inputNodeShape = re.findall(r'[\d]+\n', content[inputNodeNameLoc:inputNodeEnd])
+
+        if len(inputNodeShape) != 4:
+          print(f'Unexpected happened in shape inference, infered shape: {inputNodeShape} in node [{nodeName}]')
+          sys.exit(-1)
+
+        for i in range(len(inputNodeShape)):
+          inputNodeShape[i] = int(inputNodeShape[i].replace('\n', ''))
+        print(f'Found Node name: {nodeName}, Input Shape: {inputNodeShape}\nPatching the Mean operator...')
+
+        nodeStart =  content.rfind('{', 0, nodeNameLoc)
+        nodeEnd = content.find('node', nodeNameLoc)
+
+        if content[nodeStart:nodeEnd].find(KEEP_DIM_PATCH.replace('{KEEP_DIM}', 'false')) != -1:
+          print('Find reduce mean at top, ignore and break.')
+          endMeanNodeName = nodeName
+          break
+
+        print(f'Generating the patcher, node input: {nodeInputName}')
+        patcher = MEAN_PATCH.replace('{NAME}', nodeName)
+        patcher = patcher.replace('{INPUT}', nodeInputName)
+        patcher = patcher.replace('{KERNEL_SIZE}', str(inputNodeShape[1]))
+
+        print('Inserting patch and removing the Mean node...\n')
+        content = content[:content.rfind('node', 0 ,nodeStart)] + patcher +  content[nodeEnd:]
+
+      print('Removing unused const.\n')
+      content = re.sub(REDUCTION_IDENCE_REGEX, '', content)
+
+      while 1:
+        indecOpLoc = content.find('reduction_indices')
+        if indecOpLoc == -1:
+          break
+        indecNameLoc = content.rfind('name', 0, indecOpLoc)
+        indecStart =  content.rfind('{', 0, indecNameLoc)
+        indecEnd = content.find('node', indecNameLoc)
+        if content[indecStart:indecEnd].find(endMeanNodeName) != -1:
+          break
+        content = content[:content.rfind('node', 0 ,indecStart)] + content[indecEnd:]
+    
+    if content.find('AddV2') != -1:
+      print('Find unsupported op: AddV2, patching...\n')
+      content = content.replace('AddV2', 'Add')
 
     if content.find(KEEP_DIM_PATCH.replace('{KEEP_DIM}', 'false')) != -1:
       print('Find unsupported op: reduce_dim=false, patching...')
@@ -155,6 +276,8 @@ def pbtxt_processing(content):
       content = re.sub(U_KEY_ATTR_REGEX, '', content)
       content = re.sub(OUTPUT_SHAPE_REGEX_1, '', content)
       content = re.sub(OUTPUT_SHAPE_REGEX_2, '', content)
+
+    
 
     if content.find('op: "swish_f32"') != -1:
       print('Find unsupported op: swish_f32, patching...')
@@ -195,7 +318,7 @@ if len(sys.argv) > 2:
 
 if not os.path.isfile(os.path.join('../pbtxt/', os.path.basename(FILE_NAME).split('.')[0] + '.pbtxt')):
   with gfile.FastGFile(FILE_NAME,'rb') as f:
-      GRAPH_DEF = tf.compat.v1.GraphDef()
+      GRAPH_DEF = tf.compat.v1.get_default_graph().as_graph_def(add_shapes=True)
       GRAPH_DEF.ParseFromString(f.read())
 
   tf.import_graph_def(GRAPH_DEF, name='')
@@ -203,7 +326,7 @@ if not os.path.isfile(os.path.join('../pbtxt/', os.path.basename(FILE_NAME).spli
 else:
   PBTXT_FILE = open(os.path.join('../pbtxt/', os.path.basename(FILE_NAME).split('.')[0] + '.pbtxt'), 'r') 
 
-  GRAPH_DEF = tf.GraphDef()
+  GRAPH_DEF = tf.get_default_graph().as_graph_def(add_shapes=True)
 
   FILE_CONTENT = pbtxt_processing(PBTXT_FILE.read())
 
@@ -216,3 +339,5 @@ else:
                  os.path.dirname(FILE_NAME),
                  os.path.basename(FILE_NAME).split('.')[0] + '_patched.pb',
                  as_text=False)
+
+
